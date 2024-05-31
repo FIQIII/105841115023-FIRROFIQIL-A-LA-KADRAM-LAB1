ea837f9370c0d56df83ce9d280d9f925b02aea26
FIQIII <158143474+FIQIII@users.noreply.github.com>
2024-05-31 16:41:44 +0800

    Add files via upload
    lab 1

---
+++ qiii.js
@@ -0,0 +1,6 @@
+alert("bahaya");
+
+document.getElementById("demo").style.fontSize = "13px";
+document.getElementById("button").style.fontSize = "100px";
+document.getElementById("demo3").style.fontSize = "50px";
+
