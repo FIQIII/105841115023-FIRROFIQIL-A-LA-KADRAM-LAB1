c77c2f8d510ad649bbb2c3f363c82303630c05a5
FIQIII <158143474+FIQIII@users.noreply.github.com>
2024-06-14 23:36:40 +0800

    Update qiii.js

--- qiii.js
+++ qiii.js
@@ -1,6 +1,6 @@
 alert("bahaya");
 
 document.getElementById("demo").style.fontSize = "13px";
-document.getElementById("button").style.fontSize = "100px";
+document.getElementById("button").style.fontSize = "40px";
 document.getElementById("demo3").style.fontSize = "50px";
 
